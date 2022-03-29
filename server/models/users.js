8d3ac2f1e9a90d840788d0561b4f8cd8b21a95aa
Bety Martinez <trism460@gmail.com>
2022-03-29 16:38:24 -0600

    Update model with more atributes

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,21 @@
+const { Number } = require('mongoose');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+    id: { type: Number, required: true },
+    role: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
     name: { type: String, required: true },
-    role: { type : String, required: true },
-    password: { type : String, required: true },
-    user: { type : String, required: true },
-
+    lastName: { type: String, required: true },
+    secondLastName: {type: String, required: false },
+    age: { type: Number, required: true },
+    allergies: { type: Array, required: false },
+    otherIllnesses: { type: Array, required: false },
+    career: { type: String, required: true },
+    degree: { type: Number, required: true },
+    group: { type: CharacterData, required: true }
 });
 
 module.exports = mongoose.model('Users', UserSchema);
